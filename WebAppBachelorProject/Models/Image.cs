17a4c92fe865a3c4a5e14084a12cb26fa9ee2d00
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2024-01-31 16:38:08 +0100

    Setup

---
+++ WebAppBachelorProject/Models/Image.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppBachelorProject.Models
+{
+    public class Image
+    {
+        [Key]
+        public string Id { get; set; }
+
+
+        public string Description { get; set; }
+
+        //This must be Required, but for now let's set it like this. 
+        public string ImageUrl { get; set; }
+
+        //The foreign-key link between Image and ApplicationUser. 
+        //One-to-Many relationship.
+        public string UserId { get; set; }
+
+        //We also need to add an attribute for evaluation. 
+
+
+        //Navigation Properties: 
+        public ApplicationUser User { get; set; }
+    }
+}
