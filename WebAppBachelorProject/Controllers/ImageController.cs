9fa3a74879b15f235f9af3e3f6a120498d8d6ef4
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2024-02-05 13:02:17 +0100

    Added blank pages

---
+++ WebAppBachelorProject/Controllers/ImageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppBachelorProject.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
