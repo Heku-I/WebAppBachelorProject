83d51ed649073138b00e16beac732aab078b3abb
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2024-02-18 14:31:55 +0100

    Added "Consent" attribute on ApplicationUser.

--- WebAppBachelorProject/Models/ApplicationUser.cs
+++ WebAppBachelorProject/Models/ApplicationUser.cs
@@ -7,5 +7,8 @@ namespace WebAppBachelorProject.Models
         public string UserId { get; set; }
         public string? Firstname { get; set; }
         public string? Lastname { get; set; }
+
+        public bool Consent { get; set; } = false; 
+
     }
 }
