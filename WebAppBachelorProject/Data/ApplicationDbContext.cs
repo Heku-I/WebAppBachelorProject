ae9f90f2100fea012b23265ef854fb2532204eaa
Hekuran Ismajli <hekuran.ismajli@gmail.com>
2024-02-19 13:31:50 +0100

    Updates on DB

--- WebAppBachelorProject/Data/ApplicationDbContext.cs
+++ WebAppBachelorProject/Data/ApplicationDbContext.cs
@@ -1,14 +1,20 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WebAppBachelorProject.Models;
 
 namespace WebAppBachelorProject.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
-            Database.EnsureCreated();
+           
         }
+
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Image> Images { get; set; }
+
     }
 }
--- WebAppBachelorProject/Data/Migrations/20240219122526_Init.Designer.cs
+++ WebAppBachelorProject/Data/Migrations/20240219122526_Init.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebAppBachelorProject.Data;
 
@@ -10,9 +11,10 @@ using WebAppBachelorProject.Data;
 namespace WebAppBachelorProject.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240219122526_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.27");
